6b1e550c1993a0f7fe54e3567added0a7c2e32e1
Rohith Gurram <rohith9998@gmail.com>
2023-08-11 13:45:48 -0400

    Skeletal structure of Phone-Directory

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@
 function App() {
   return (
     <div>
-      
+      <div className="Header">
+        <input type="text" placeholder="Your Name"></input>
+      </div>
+      <button> Add </button>
+      <div>
+        <span> Name </span> <br />
+        <span> Phone </span>
+      </div>
     </div>
   );
 }
